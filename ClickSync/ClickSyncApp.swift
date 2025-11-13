919e55c08187a950a283c35683846d27c53eb109
George <georgecasartelli@gmail.com>
2025-11-13 13:22:51 +0000

    Initial Commit

---
+++ ClickSync/ClickSyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClickSyncApp.swift
+//  ClickSync
+//
+//  Created by George Casartelli on 13/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct ClickSyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
