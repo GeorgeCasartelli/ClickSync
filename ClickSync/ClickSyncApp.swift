c30f9367f45e5a09ee65fb95a60d1b91abf702e2
GeorgeCasartelli <102063273+GeorgeCasartelli@users.noreply.github.com>
2026-01-19 12:02:28 +0000

    Update ClickSyncApp.swift

--- ClickSync/ClickSyncApp.swift
+++ ClickSync/ClickSyncApp.swift
@@ -5,6 +5,11 @@
 //  Created by George Casartelli on 13/11/2025.
 //
 
+// AI Use (Disclosure):
+// AI has been used in conjunction with this project for small drafting of sections, UI development and occasional debugging.
+// Everything has bee reviewed, adapted, and tested by a hell of a lot me before the final submission lol.
+
+
 import SwiftUI
 
 @main
